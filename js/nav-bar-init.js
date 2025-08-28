90aabeab6ac5467f100f1e8cf0da4d10d11a2708
nic3n1217 <nic3n1217@gmail.com>
2025-08-28 16:47:23 +0000

    se agrego el contactenos, se reparo el registro y se esta trabajado en el home

--- js/nav-bar-init.js
+++ js/nav-bar-init.js
@@ -1,6 +1,6 @@
 function open_close_burger () {
     let burger_menu = document.getElementById("burger-menu");
     let burger_button = document.getElementById("button-burger");
-    burger_menu.classList.toggle("burger-menu-active");
+    burger_menu.classList.gltoge("burger-menu-active");
     burger_button.classList.toggle("button-burger-active");
     }
